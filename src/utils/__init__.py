ac289440f1eae0cbaf3c8477aaf1423119002a78
MellMatsuda <matsudamell@gmail.com>
2025-10-26 17:40:19 -0300

    Fix Git LFS issues: checkout LFS objects to resolve pointer files

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e31cad0b53e0dd70419e8ef1ae9c3f9ce22e58f0237caeb35c87b153ff6b3a00
-size 127
+from .helpers import get_project_root
+from .elt import build_row_hash
+from .atc_normalize import normalize_principio_ativo_atc
